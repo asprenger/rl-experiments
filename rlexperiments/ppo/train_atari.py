905e751ee03cc940975f1b18df4c09f08b697b10
asprenger <info@andresprenger.de>
2018-05-01 22:34:15 +0200

    Fix video support for PPO train atari

--- rlexperiments/ppo/train_atari.py
+++ rlexperiments/ppo/train_atari.py
@@ -1,9 +1,10 @@
-
+import os
 import argparse
 import tensorflow as tf
 from gym.wrappers.monitor import Monitor
 from rlexperiments.common.atari_utils import make_atari, wrap_deepmind
-from rlexperiments.common.tf_util import create_session, ensure_dir
+from rlexperiments.common.tf_util import create_session
+from rlexperiments.common.util import ensure_dir
 from rlexperiments.vec_env.subproc_vec_env import SubprocVecEnv
 from rlexperiments.vec_env.vec_frame_stack import VecFrameStack
 from rlexperiments.vec_env.episode_monitor import EpisodeMonitor
@@ -29,9 +30,9 @@ def make_atari_env(env_id, num_env, seed, output_dir, record_video, record_video
     return EpisodeMonitor(venv)
 
 
-def train(env_id, num_envs, num_timesteps, seed, cuda_visible_devices, gpu_memory_fraction, output_dir):
+def train(env_id, num_envs, num_timesteps, seed, cuda_visible_devices, gpu_memory_fraction, output_dir, video):
 
-    env = make_atari_env(env_id, num_envs, seed, output_dir='', record_video=False)
+    env = make_atari_env(env_id, num_envs, seed, output_dir=output_dir, record_video=video)
     env = VecFrameStack(env, 4)
     policy = CnnPolicy
 
@@ -53,9 +54,11 @@ def main():
     parser.add_argument('--cuda-visible-devices', help='comma separated list of GPU IDs used by CUDA', default='0')
     parser.add_argument('--gpu-memory-fraction', help='fraction of GPU memory used', type=float, default=0.5)
     parser.add_argument('--output-dir', help='base directory to store models and summaries', default='/tmp/train/ppo')
+    parser.add_argument('--video', help='enable video recording', action='store_true')
+
     args = parser.parse_args()
     train(args.env, num_envs=args.num_envs, num_timesteps=args.num_timesteps, seed=0, cuda_visible_devices=args.cuda_visible_devices, 
-        gpu_memory_fraction=args.gpu_memory_fraction, output_dir=args.output_dir)
+        gpu_memory_fraction=args.gpu_memory_fraction, output_dir=args.output_dir, video=args.video)
 
 if __name__ == '__main__':
     main()
